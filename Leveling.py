2055c5abd853556871db35327547b505f80b784b
rttf <74861784+RTF47@users.noreply.github.com>
2025-04-23 16:34:45 +0500

    Update Leveling.py

--- Leveling.py
+++ Leveling.py
@@ -1,12 +1,11 @@
 import pygame
-import numpy as np
 from Obects import Wall, Water, Ground
 
-class Grid():
+class Grid:
     def __init__(self, x, y, screen):
-        self.firstgrid =           [['w','w','w','w','w','w','w','w','w','w''w','w','w','w'],
+        self.firstgrid =           [['w','w','w','w','w','w','w','w','w','w','w','w','w','w'],
                                    ['w','w','w','w','w','w','w','w','w','w','w','w','w','w'],
-                                   ['w','w','fw','fw','fw','fw','fw','fw','fw','fw','fw','fw','w','w'],
+                                   ['w','w','tw','tw','tw','tw','tw','tw','tw','tw','tw','tw','w','w'],
                                    ['w','w','sw','g','g','g','g','g','g','g','g','sw','w','w'],
                                    ['w','w','sw','g','g','g','g','g','g','g','g','sw','w','w'],
                                    ['w','w','sw','g','g','g','g','g','g','g','g','sw','w','w'],
@@ -21,19 +20,31 @@ class Grid():
         self.screen = screen
         self.x = x
         self.y = y
-        self.water = Water(self.x, self.y, self.screen)
-        self.wall = Wall(self.x, self.y, self.screen)
-        self.ground = Ground(self.x, self.y, self.screen)
+        self.groupofrects = []
 
 
     def drawing(self):
         for row in self.firstgrid:
+            if self.y == 576 and self.x == 576:
+                self.y = 128
+                self.groupofrects = []
             for col in row:
+                if self.x == 576:
+                    self.x = 128
                 if col == 'w':
-                    self.water.draw()
+                    # print(self.x, self.y)
+                    water = Water(self.x, self.y, self.screen)
+                    self.groupofrects.append(water.rect)
+                    water.draw()
                 elif col == 'tw' or col == 'sw':
-                    self.wall.draw()
+                    # print(self.x, self.y)
+                    wall = Wall(self.x, self.y, self.screen)
+                    self.groupofrects.append(wall.rect)
+                    wall.draw()
                 elif col == 'g':
-                    self.ground.draw()
+                    # print(self.x, self.y)
+                    ground = Ground(self.x, self.y, self.screen)
+                    ground.draw()
                 self.x += 32
             self.y += 32
+        return
