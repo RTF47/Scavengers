34768215fda04ff53ee75039ed8aa9b10c8e1873
rttf <74861784+RTF47@users.noreply.github.com>
2025-04-23 16:34:19 +0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,43 +1,53 @@
 import pygame
-
+"""
+https://gamedevacademy.org/pygame-collisions-tutorial-complete-guide/
+"""
 
 class Player:
     def __init__(self,screen):
         pygame.init()
-        self.x = 400
-        self.y = 300
-        self.leftspeed = 1
-        self.rightspeed = 1
-        self.upspeed = 1
-        self.downspeed = 1
-        self.rect =pygame.Rect(self.x, self.y, 60, 60)
+        self.x = 256
+        self.y = 256
+        self.leftspeed = 4
+        self.rightspeed = 4
+        self.upspeed = 4
+        self.downspeed = 4
+        self.rect =pygame.Rect(self.x, self.y, 32, 32)
         self.screen = screen
 
 
     def input(self):
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: self.y -= self.upspeed
-        if pressed[pygame.K_DOWN]: self.y += self.downspeed
-        if pressed[pygame.K_RIGHT]: self.x += self.rightspeed
-        if pressed[pygame.K_LEFT]: self.x -= self.leftspeed
-
-        self.rect.x = self.x
-        self.rect.y = self.y
+        if pressed[pygame.K_UP]: self.rect.y -= self.upspeed
+        if pressed[pygame.K_DOWN]: self.rect.y += self.downspeed
+        if pressed[pygame.K_RIGHT]: self.rect.x += self.rightspeed
+        if pressed[pygame.K_LEFT]: self.rect.x -= self.leftspeed
 
 
     def draw(self):
         pygame.draw.rect(self.screen,(0, 0, 0), self.rect)
 
 
-    def collision(self):
-        if self.rect.right > 800:
-            self.rect.right = 800
+    def collision(self, anotherrect):
+        if self.rect.right > 1280:
+            self.rect.right = 1280
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.top < 0:
             self.rect.top = 0
-        if self.rect.bottom > 600:
-            self.rect.bottom = 600
+        if self.rect.bottom > 720:
+            self.rect.bottom = 720
+
+        # if self.rect.colliderect(anotherrect):
+        #     if self.rect.right > anotherrect.left:
+        #         self.rect.right = anotherrect.left
+        #     if self.rect.left > anotherrect.right:
+        #         self.rect.left = anotherrect.right
+        #     elif self.rect.top > anotherrect.bottom:
+        #         self.rect.top = anotherrect.bottom
+        #     elif self.rect.bottom > anotherrect.top:
+        #         self.rect.bottom = anotherrect.top
+
         # pressed = pygame.key.get_pressed()
         # if obs_type == 1 and self.x in range(pos[0]-70, pos[1]+16) and self.y in range(pos[2]-60, pos[3]+1):
         #     self.x = 400
