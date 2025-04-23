4ab7d9fa53cdd866be94e67694f0de040f6a92c6
rttf <74861784+RTF47@users.noreply.github.com>
2025-04-23 14:04:00 +0500

    Update Leveling.py
    the output logic has been changed

--- Leveling.py
+++ Leveling.py
@@ -4,7 +4,7 @@ from Obects import Wall, Water, Ground
 
 class Grid():
     def __init__(self, x, y, screen):
-        self.firstgrid = [['w','w','w','w','w','w','w','w','w','w''w','w','w','w'],
+        self.firstgrid =           [['w','w','w','w','w','w','w','w','w','w''w','w','w','w'],
                                    ['w','w','w','w','w','w','w','w','w','w','w','w','w','w'],
                                    ['w','w','fw','fw','fw','fw','fw','fw','fw','fw','fw','fw','w','w'],
                                    ['w','w','sw','g','g','g','g','g','g','g','g','sw','w','w'],
@@ -21,21 +21,19 @@ class Grid():
         self.screen = screen
         self.x = x
         self.y = y
+        self.water = Water(self.x, self.y, self.screen)
+        self.wall = Wall(self.x, self.y, self.screen)
+        self.ground = Ground(self.x, self.y, self.screen)
 
 
     def drawing(self):
         for row in self.firstgrid:
             for col in row:
                 if col == 'w':
-                    water = Water(self.x, self.y, self.screen)
-                    water.draw()
-                if col == 'tw' or col == 'sw':
-                    wall = Wall(self.x, self.y, self.screen)
-                    wall.draw()
-                if col == 'g':
-                    ground = Ground(self.x, self.y, self.screen)
-                    ground.draw()
+                    self.water.draw()
+                elif col == 'tw' or col == 'sw':
+                    self.wall.draw()
+                elif col == 'g':
+                    self.ground.draw()
                 self.x += 32
             self.y += 32
-
-
