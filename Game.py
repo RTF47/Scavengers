976c580c66f7ef411dd35f7942b87f9a417b9a36
rttf <74861784+RTF47@users.noreply.github.com>
2025-04-23 16:33:55 +0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import pygame
 from Leveling import Grid
+from Player import Player
 
 class Game:
     def __init__(self) -> None:
@@ -8,6 +9,7 @@ class Game:
         self.screen = pygame.display.set_mode((1280,720))
         self.level = Grid(128, 128, self.screen)
         self.clock = pygame.time.Clock()
+        self.player = Player(self.screen)
 
 
     def ProcessInput(self) -> None:
@@ -15,23 +17,21 @@ class Game:
             if event.type == pygame.QUIT:
                 self.going = True
 
-        # self.player.input()
+        self.player.input()
 
 
     def Update(self) -> None:
-        pass
+        for i in self.level.groupofrects:
+            self.player.collision(i)
 
 
     def Render(self) -> None:
         self.screen.fill((255, 255, 255))
-        #player
         self.level.drawing()
+        self.player.draw()
         pygame.display.flip()
 
 
     def SwitchToScene(self, next_scene) -> None:
         #self.next = next_scene
         pass
-
-
-
