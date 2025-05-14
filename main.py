9c13b9512c4b27554bcd613674ab4ba47fcac433
rttf <74861784+RTF47@users.noreply.github.com>
2025-05-15 01:56:16 +0500

    Новые изменения

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-import pygame
-from Game import Game
+import pygame, sys
+from settings import *
+from level import Level
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Buccaneers")
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
 
 if __name__ == '__main__':
     game = Game()
-    while not game.going:
-        game.ProcessInput()
-        game.Update()
-        game.Render()
-        game.clock.tick(60)
-    pygame.quit()
+    game.run()
