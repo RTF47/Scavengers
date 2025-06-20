46866e21259cff6896eef2699b126da0d66742ca
rttf <74861784+RTF47@users.noreply.github.com>
2025-06-20 11:46:34 +0500

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -1,83 +1,118 @@
 """НЕКОТОРЫЕ НАСТРОЙКИ/ПЕРЕМЕННЫЕ, ЧТОБЫ ИЗБЕЖАТЬ MAGIC_CONST"""
-
+#Параметры игры
 WIDTH = 1280
 HEIGHT = 720
 FPS = 60
 FPS_PAUSE = 10
-TILE_SIZE = 64
-ITEM_BOX_SIZE = 80
-FRAME_SPEED = 0.15
-FRAME_IND = 0
-MAP = '../graphics/tilemap/Map3.png'
-LOGO_PATH = '../graphics/runes/6-32x32.png'
 
-PLAYER_IMG = '../graphics/player/down_idle/idle_down.png'
-PLAYER_PATH = '../graphics/player/'
-HEALTH = 6
-POWER = 10
-P_SPEED = 6
+#Параметры анимации
+FRAME_SPEED = 0.15 #Скорость переключения кадров анимации
+FRAME_IND = 0 #Начальный индекс кадров
 
-TITLE_OFFSET = 120
+#Параметры игрока
+HEALTH = 6 #ХП
+POWER = 10 #Атака
+P_SPEED = 6 #Скорость ходьбы
 
-FINISH_POS = (1280, 640)
-FINISH_SIZE = 128
+#Параметры руны
+RUNE_RADIUS = 100 #Радиус притягивания руны
+RUNE_SPEED = 3 #Скорость притягивания руны
 
-OVERLAY_POS = (0,0)
-
+#POSITIONS
+PROMPT_POS = (100, 100)
+NAME_SURF_POS = (100, 150) #Позиция поля для ввода имени
+WARN_POS = (100, 200) #Позиция предупреждения на экране
+TIMER_POS = (WIDTH - 150, 10) #Позиция таймера на экране
+WEAPON_POS = (10, 630) #Позиция UI оружия на экране
+HEART_POS = (10,15) #Позиция сердечек на экране
+RUNE_UI_POS = (200, 10) #Позиция UI Руны на экране
+CHEST_POS = (1216, 1792) #Позиция сундука на карте
+OVERLAY_POS = (0,0) #Позиция оверлея, то есть самое начало
 DEATH_POS = (WIDTH // 3 + 55, HEIGHT // 2 - 110)
+LEADERBOARD_POS = (800,340) #Позиция лидерборда в меню
+FINISH_POS = (1280, 640) #Позиция финиша
 
-FONT_PATH = '../graphics/font/alagard-12px-unicode.ttf'
-FONT_SIZE = 120
-SMALL_FONT_PATH = '../graphics/font/joystix.ttf'
-SMALL_FONT_SIZE = 30
-UI_FONT = SMALL_FONT_PATH
-UI_FONT_SIZE = 18
+#SIZES
+BUTTON_SIZE = (250, 64) #Размер кнопок
+HEART_SIZE = (25,24) #Размер сердечек
+CHEST_SIZE = (128,128) #Размер сундука
+FINISH_SIZE = 128 #Размер квадратной зоны финиша
+TILE_SIZE = 64 #Размер тайла игры
+ITEM_BOX_SIZE = 80 #Размер UI бокса с оружием
+SBOX_WIDTH = 3 #Ширина обводки UI бокса
+FONT_SIZE = 120 #Размер титульного шрифта
+SMALL_FONT_SIZE = 30 #Размер основного шрифта
+UI_FONT_SIZE = 18 #Размер шрифта UI
 
-LEADERBOARD_POS = (800,340)
+#OFFSETS
+Y_OFFSET = 30 #Смещение по Y для кнопки play
+OFFSETXY = 130 #Смещение по X и Y для кнопки play и quit
+RMX_OFFSET = 35 #Смещение по X для кнопки resume и menu
+MY_OFFSET = 100 #Смещение по Y для кнопки menu
+LB_OFFSET = 60 #Смещение по Y для Toп5 после You:
+TOP_OFFSET = 50 #Смещение по Y для You: после Best Time
+TITLE_OFFSET = 120 #Смещение названия игры относительно y
+HEART_GAP = 5 #Смещение каждого сердечка на промежуток
 
-CHEST_SIZE = (128,128)
-CHEST_FOLDER = '../graphics/chest'
-CHEST_POS = (1216, 1792)
-CHEST_OPEN_S = '../audio/chest.mp3'
+#CSV ids
+NOTHING_ID = '-1' #Буквально - айди ничего
+PLAYER_ID = '394' #Айди игрока
+BAMBOO_ID = '390' #Айди врага бамбук
+RACCOON_ID = '392' #Айди врага енот
+SPIRIT_ID = '391' #Айди врага дух
+
+#Все пути, графика, карта
+CLAW_F = '../graphics/particles/claw' #кадры атаки енота
+SLASH_F = '../graphics/particles/slash' #кадры атаки игрока
+SPARKLE_F = '../graphics/particles/sparkle' #кадры атаки сквида
+LEAF_F = '../graphics/particles/leaf_attack'  #кадры атаки бамбука
+THUNDER_F = '../graphics/particles/thunder' #кадры атаки духа
+SQUID_D = '../graphics/particles/smoke_orange' #Графика сквида
+RACCOON_D = '../graphics/particles/raccoon' #Графика енота
+SPIRIT_D = '../graphics/particles/nova' #Графика духа
+BAMBOO_D = '../graphics/particles/bamboo' #Графика бамбука
+BOUNDARY_CSV = '../map/map_Boundary.csv' #Карта границ
+ENTITY_CSV = '../map/map_Entitys.csv' #Карта энтити
+PLAY_B = '../graphics/buttons/Play.png' #Кнопка play
+MENU_B = '../graphics/buttons/Menu.png' #Кнопка menu
+QUIT_B = '../graphics/buttons/Quit.png' #Кнопка quit
+RESUME_B ='../graphics/buttons/Resume.png' #Кнопка resume
+REPLAY_B = '../graphics/buttons/Replay.png' #Кнопка replay
+F_HEART = "../graphics/HP/OneHeart.png" #full - полный, полное сердечко
+E_HEART = "../graphics/HP/VoidHeart.png" #empty - пустой, пустое сердечко
+RUNE_PATH = '../graphics/runes/1-32x32.png' #Обычная руна
+E_RUNE_PATH = "../graphics/runes/dark1-32x32.png" #empty - пустой, пустая руна
+MAP = '../graphics/tilemap/Map3.png' #Карта, по которой ходит игрок
+LOGO_PATH = '../graphics/runes/6-32x32.png' #Логотип игры
+PLAYER_IMG = '../graphics/player/down_idle/idle_down.png' #Картинка игрока
+PLAYER_PATH = '../graphics/player/' #Папка со всеми взаимодействиями игрока
+CHEST_FOLDER = '../graphics/chest' #Папка с кажрами сундука
+
+#Все пути, звуки. Приписка _S означает sound
+MAIN_S = '../audio/main.ogg' #музыка
+WIN_S = '../audio/win.wav' #звук победы
+GO_S = '../audio/death.wav' #звук проигрыша
+DEATH_S = '../audio/death.wav' #звук смерти
+HIT_S = '../audio/hit.wav' #звук попадания
+SWORD_S = '../audio/sword.wav' #звук удара
+CHEST_OPEN_S = '../audio/chest.mp3' #звук открытия сундука
+
+#Все пути, шрифты.
+SMALL_FONT_PATH = '../graphics/font/joystix.ttf' #Основной шрифт
+FONT_PATH = '../graphics/font/alagard-12px-unicode.ttf' #Титульный шрифт
+UI_FONT = SMALL_FONT_PATH #UI шрифт
+
+#Громкости звуков и каналы. Приписка _V значает volume
+CHANNELS = 16 #количество каналов, чтобы все звуки проигрывались
+MAIN_V = 0.05
+WIN_V = 0.2
+GO_V = 0.4
+GO_CHANNEL = 7 #Закрепляем за звуком проигрыша свой канал, иначе не работает
+ENEMY_V = 0.1 #Общая громкость врага
+SWORD_V = 0.2
 CHEST_OPEN_V = 0.2
 
-RUNE_PATH = '../graphics/runes/1-32x32.png'
-E_RUNE_PATH = "../graphics/runes/dark1-32x32.png"
-RUNE_RADIUS = 100
-RUNE_SPEED = 3
-RUNE_UI_POS = (200, 10)
-
-F_HEART = "../graphics/HP/OneHeart.png"
-E_HEART = "../graphics/HP/VoidHeart.png"
-HEART_SIZE = (25,24)
-HEART_POS = (10,15)
-HEART_GAP = 5
-
-SBOX_WIDTH = 3
-WEAPON_POS = (10, 630)
-
-ATTACK_CD = 400
-INVICIBILITY_DUR = 300
-INVULN_CD = 500
-CHANGE_CD = 200
-
-LB_OFFSET = 60
-TOP_OFFSET = 50
-
-TIMER_POS = (WIDTH - 150, 10)
-RMX_OFFSET = 35
-MY_OFFSET = 100
-
-PROMPT_POS = (100, 100)
-NAME_SURF_POS = (100, 150)
-WARN_POS = (100, 200)
-
-OVERLAY_FILL = (50,50,50)
-
-OFFSETXY = 130
-BUTTON_SIZE = (250, 64)
-Y_OFFSET = 30
-
+#Цвета
 WATER_COLOR = '#71ddee'
 UI_BG_COLOR = '#222222'
 UI_BORDER_COLOR = '#111111'
@@ -86,45 +121,16 @@ UI_BORDER_COLOR_ACTIVE = 'gold'
 WHITE = (255,255,255)
 YELLOW = (255,255,0)
 RED = (255,0,0)
+OVERLAY_FILL = (50,50,50)
 
-CHANNELS = 16
-MAIN_S = '../audio/main.ogg'
-MAIN_V = 0.05
-WIN_S = '../audio/win.wav'
-WIN_V = 0.2
-GO_S = '../audio/death.wav'
-GO_V = 0.4
-GO_CHANNEL = 7
-DEATH_S = '../audio/death.wav'
-HIT_S = '../audio/hit.wav'
-ENEMY_V = 0.1
-SWORD_S = '../audio/sword.wav'
-SWORD_V = 0.2
+#Всё, что связано с боем
+ATTACK_CD = 400 #Время, еоторое проходит до следующей атаки
+INVICIBILITY_DUR = 300 #время, которое проходит, после удара игрока
+INVULN_CD = 500 #время, которое проходит, после удара врага
+CHANGE_CD = 200 #Время, которое проходит до еще одной смены оружия
 
-PLAY_B = '../graphics/buttons/Play.png'
-MENU_B = '../graphics/buttons/Menu.png'
-QUIT_B = '../graphics/buttons/Quit.png'
-RESUME_B ='../graphics/buttons/Resume.png'
-REPLAY_B = '../graphics/buttons/Replay.png'
 
-BOUNDARY_CSV = '../map/map_Boundary.csv'
-ENTITY_CSV = '../map/map_Entitys.csv'
-NOTHING_ID = '-1'
-PLAYER_ID = '394'
-BAMBOO_ID = '390'
-RACCOON_ID = '392'
-SPIRIT_ID = '391'
-
-CLAW_F = '../graphics/particles/claw'
-SLASH_F = '../graphics/particles/slash'
-SPARKLE_F = '../graphics/particles/sparkle'
-LEAF_F = '../graphics/particles/leaf_attack'
-THENDER_F = '../graphics/particles/thunder'
-SQUID_D = '../graphics/particles/smoke_orange'
-RACCOON_D = '../graphics/particles/raccoon'
-SPIRIT_D = '../graphics/particles/nova'
-BAMBOO_D = '../graphics/particles/bamboo'
-
+#Цельные характеристики
 weapon_data = {
 	'sword': {'cooldown': 100, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
 	'lance': {'cooldown': 400, 'damage': 30,'graphic':'../graphics/weapons/lance/full.png'},
